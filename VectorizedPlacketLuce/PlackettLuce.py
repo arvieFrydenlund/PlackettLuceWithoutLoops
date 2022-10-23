Added model training example.  Found numerical stability issue when cost is very low and not sure how to fix it.

VectorizedPlacketLuce/PlackettLuce.py
@@ -6,12 +6,23 @@ from torch import nn as nn
 
 
 class PlackettLuceLoss(nn.Module):
+    """
+    Partial Plackett-Luce
+    """
     def __init__(self):
         super(PlackettLuceLoss, self).__init__()
         self.ninf = -np.inf
 
     @staticmethod
-    def _avg(loss, target_lengths=None, num=None):
+    def avg(loss, target_lengths=None, num=None, reduce='none'):
+        """
+        :param loss: [d1...dn-1, max_k]
+        :param target_lengths:
+        :param num:
+        :return: pl_avg_loss, [1],
+                 pl_loss, [d1...dn-1], summed across the ranks
+                 [1], number of values
+        """
         if num is not None:
             avg_loss = loss.sum() / num
         elif target_lengths is not None:
@@ -20,7 +31,8 @@ class PlackettLuceLoss(nn.Module):
         else:
             num = torch.ones([1]) * loss.nelement()
             avg_loss = loss.mean()
-        loss = torch.sum(loss, dim=-1)  # [-1]
+        if reduce == 'ranks':
+            loss = torch.sum(loss, dim=-1)  # [-1]
         return avg_loss, loss, num
 
     @staticmethod
@@ -32,17 +44,22 @@ class PlackettLuceLoss(nn.Module):
         return bool(torch.isnan(t).any())
 
     @staticmethod
-    def _bad_check(t):
-        return PlackettLuceLoss._is_inf_check(t) or PlackettLuceLoss._nan_check(t)
+    def bad_loss_check(t):
+        is_inf = PlackettLuceLoss._is_inf_check(t)
+        is_nan = PlackettLuceLoss._nan_check(t)
+        if is_inf or is_nan:
+            raise ValueError(f'Bad loss: is inf {is_inf}, is nan {is_nan}, check numerical stability value.')
 
-    def loop_forward(self, logits, pl_targets):
+    def loop_forward(self, logits, pl_targets, reduce='none'):
         """
-        For comparison with vectorized version
+        Wasteful version that needs to loop over the ranks and recompute the normalization term each iteration.
+        For comparison with vectorized version.
+        Note, that the most preferred rank will be equal to regular cross-entropy softmax.
         :param logits: [d1...dn-1, vocab_size], float32
         :param pl_targets: [d1...dn-1, max_k], ranked order indices, int64
-        :return:
+        :return:  [d1...dn-1, max_k]
         """
-        masked_logits = logits.clone()
+        masked_logits = logits.clone()  # we can't modify the actual logits otherwise backwards doesn't make sense.
         pl_full_loss = []
         ninf = torch.ones_like(logits) * self.ninf
         for k in range(pl_targets.shape[-1] - 1):
@@ -50,36 +67,43 @@ class PlackettLuceLoss(nn.Module):
             masked_logits.scatter_(dim=-1, index=pl_targets[..., k].unsqueeze(dim=-1), src=ninf)
         pl_full_loss.append(torch.nn.functional.cross_entropy(masked_logits, pl_targets[..., -1], reduction='none'))
         pl_full_loss = torch.stack(pl_full_loss, dim=-1)
-        pl_avg_loss, pl_loss, pl_num = self._avg(pl_full_loss)
-        return pl_avg_loss, pl_loss, pl_num,  pl_full_loss
+        return self.avg(pl_full_loss, reduce=reduce)
 
-    def forward(self, logits, pl_targets, target_lengths=None, target_values=None, orders=None, **kwargs):
+    def forward(self, logits, pl_targets, target_lengths=None, target_values=None, orders=None,
+                reduce='none', **kwargs):
         """
-        :param logits:  [d1...dn-1, vocab_size], float32
+        Note the eta term will make it different from the loop version when the cost becomes very low.
+        This will then up weight the lowest ranks, so be careful if you see the loss increasing.
+        Otherwise, set eta to zero and do nan check when cost becomes low.
+
+        :param logits:  [d1...dn-1, vocab_size], float32, class scores
         :param pl_targets: [d1...dn-1, max_k], ranked order indices, int64
         :param target_lengths: [d1...dn-1], the dynamic size of k, int64 or None
         :param target_values: [d1...dn-1, max_k], weights, float32
-        :param orders: [d1...dn-1, max_k], weights, int64
-        :return: [1], [d1...dn-1, max_k], [1]
+                              Currently we assume that these are normalized to 1 over the ranks.  TODO
+        :param orders: [d1...dn-1, max_k], int64, in range (0, max_k) or None
+                        these are indices to the orders 'head' i.e. the first element of the order
+                        so for example, [0, 1, 1, 3, 3, 5, 5, 5, 5, 9]
+                        means the gt is 0, then we have an order of two words, with the head at index 1,
+                        then an order of 2 things with the head at 3, then an order of four things with the head at 5.
+        :param reduce: str, ('none', 'rank')
+        :return: pl_avg_loss, [1],
+                 pl_loss, [d1...dn-1], summed across the ranks if reduce = 'ranks'
+                          [d1...dn-1, max_k], if reduce = 'none'
+                 num [1], number of values (possibly weighted by target_values)
         """
 
         pl_full_loss, mask = self._forward(logits, pl_targets, target_lengths, orders=orders)
-        if target_values is not None:
+        if target_values is not None:  # weight loss by target values
             pl_full_loss *= target_values.view(-1, target_values.shape[-1])
 
-        if self._nan_check(pl_full_loss) or self._is_inf_check(pl_full_loss):
-            raise ValueError('NAN in no_ce_forward')
-
         if target_values is not None:  # assume normalized target values
-            pl_avg_loss, pl_loss, pl_num = self._avg(pl_full_loss, mask, num=mask.shape[0])
+            return self.avg(pl_full_loss, mask, num=mask.shape[0], reduce=reduce)
         else:
-            pl_avg_loss, pl_loss, pl_num = self._avg(pl_full_loss, mask)
-
-        return pl_avg_loss, pl_loss, pl_num, pl_full_loss
-
+            return self.avg(pl_full_loss, mask,  reduce=reduce)
 
     @staticmethod
-    def _forward(logits, pl_targets, target_lengths=None, orders=None, **kwargs):
+    def _forward(logits, pl_targets, target_lengths=None, orders=None, eta=0.000001, **kwargs):
         """
         Note that we need to calculate the logits and normalization term, Z, only once, then modify Z
         and renormalize at every instance of the 'loop':
@@ -97,7 +121,8 @@ class PlackettLuceLoss(nn.Module):
                         so for example, [0, 1, 1, 3, 3, 5, 5, 5, 5, 9]
                         means the gt is 0, then we have an order of two words, with the head at index 1,
                         then an order of 2 things with the head at 3, then an order of four things with the head at 5.
-        :return: [d1...dn-1, max_k]
+        :param eta: small float for numerical stability
+        :return: [d1...dn-1, max_k], [d1...dn-1, max_k] or None
         """
 
         max_k = pl_targets.shape[-1]  # max_k needs to be >= 2, otherwise just use cross entropy
@@ -131,9 +156,16 @@ class PlackettLuceLoss(nn.Module):
             Z_mod = torch.gather(Z_mod, dim=-1, index=orders)
 
         # need small num for numerical stability, else -inf
-        # note this will make it different from forward method
-        Z = torch.log(Z - Z_mod + 0.000001) + m  # [-1 max_k], log renormed Z + log-sum-exp trick
+        # note this will make it different from the loop method
+        # these are only needed for the lower ranks, so we can use smaller eta to make the top ranks the same
+        eta_range = torch.arange(0, max_k, device=logits.device).float()
+        eta_range *= (eta / max_k)
+
+        Z = torch.log(Z - Z_mod + eta_range) + m  # [-1 max_k], log renormed Z + log-sum-exp trick
         loss = Z - logits_gather  # [-1 max_k]
+
+        if max_k == logits.shape[-1]:  # fix for full rankings, should be a better way of doing this
+            loss.scatter_(-1, index=torch.ones_like(pl_targets)*(max_k-1), src=torch.zeros_like(loss))
 
         mask = None
         if target_lengths is not None:
@@ -141,17 +173,6 @@ class PlackettLuceLoss(nn.Module):
                     < target_lengths[:, None].int()).float()
 
             fill_mask = mask <= 0.0
-            # fill_mask |= self._bad_mask(torch.log(Z - Z_mod))
             loss.masked_fill_(fill_mask, 0.0)
         return loss, mask
-
-    def to_str(self, loss, num, ce_loss, ce_num, *args, **kwargs):
-        pl_avg_loss = float(loss / num)
-        ce_avg_loss = float(ce_loss / ce_num)
-        ppl = math.exp(ce_avg_loss)
-        bpc = ce_avg_loss / math.log(2)
-
-        s = 'PL loss {:5.4f} | '.format(pl_avg_loss)
-        s += 'CE loss {:5.4f} | ppl {:8.4f} | bpc {:8.4f} | '.format(ce_avg_loss, ppl, bpc)
-        return s, pl_avg_loss, ce_avg_loss, ppl, bpc
 

VectorizedPlacketLuce/test.py
@@ -11,7 +11,7 @@ def make_targets(bs, k, v):
     return torch.from_numpy(np.asarray(targets)).long()
 
 
-def test():
+def test_forward_back_with_loop():
 
     bs = 3
     k = 7
@@ -23,27 +23,44 @@ def test():
     loop_logits = logits.clone()
     pl_targets = make_targets(bs, k, v)
 
-    pl_avg_loss, pl_loss, pl_num, pl_full_loss = pl(logits, pl_targets)
-    print(pl_loss.shape, logits.shape, pl_targets.shape, pl_full_loss.shape)
-    print(pl_full_loss)
+    pl_avg_loss, pl_full_loss, _ = pl(logits, pl_targets, reduce='none')
+    print('\t\t', pl_full_loss)
 
-    loop_pl_avg_loss, loop_pl_loss, loop_pl_num, loop_pl_full_loss = pl.loop_forward(loop_logits, pl_targets)
-    print(loop_pl_loss.shape, loop_logits.shape, pl_targets.shape, loop_pl_full_loss.shape)
-    print(loop_pl_full_loss)
+    loop_pl_avg_loss, loop_pl_full_loss, _ = pl.loop_forward(loop_logits, pl_targets)
+    print('\t\t', loop_pl_full_loss)
 
     print(f'Forward all the same: {torch.allclose(pl_full_loss, loop_pl_full_loss)}')
 
-    print(pl_avg_loss, loop_pl_avg_loss)
+    print('\t\t', pl_avg_loss, loop_pl_avg_loss)
 
     torch.autograd.backward(pl_avg_loss, inputs=logits)
     torch.autograd.backward(loop_pl_avg_loss, inputs=loop_logits)
 
-    print(logits.grad)
-    print(loop_logits.grad)
+    print('\t\t', logits.grad)
+    print('\t\t', loop_logits.grad)
 
     print(f'Backward all the same: {torch.allclose(logits.grad, loop_logits.grad)}')
 
 
+def test_dynamic_lengths():
+    pass
+
+def test_multidim():
+    pass
+
+
+def test_full_ranking():
+    pass
+
+
+def test_target_values():
+    pass
+
+
+def test_orders():
+    pass
+
+
 
 if __name__ == '__main__':
-    test()
+    test_forward_back_with_loop()

VectorizedPlacketLuce/train.py
@@ -1,6 +1,90 @@
-#  https://www.microsoft.com/en-us/research/project/letor-learning-rank-information-retrieval/letor-4-0/
+import numpy as np
+import torch
+from torch import nn
+from torch.optim import SGD
 
+from VectorizedPlacketLuce.PlackettLuce import PlackettLuceLoss
+
+
+#  TODO rank auto encoder?
+
+class RankModel(nn.Module):
+    """
+    Dumb model which tries to arrange the inputs in order by scoring each item.
+    Thus, the embeddings should produce scores that are ordered.
+    Note that the inputs are the classes, so this should be easy.
+    """
+    def __init__(self, num_class=19, max_k=11, dim=256):
+        super(RankModel, self).__init__()
+        self.num_class = num_class
+        self.max_k = max_k
+        self.dim = dim
+
+        self.embeddings = nn.Embedding(num_class, dim)
+        self.score = nn.Linear(dim, 1, bias=False)
+        nn.init.uniform_(self.embeddings.weight, -.01, .01)
+
+    def forward(self, inputs):
+        """
+        :param inputs: [bs, max_k < dim]
+        :return:
+        """
+        return self.score(nn.functional.tanh(self.embeddings(inputs))).squeeze(dim=-1)
+
+    @staticmethod
+    def make_inputs_targets(bs, k, v, _print=False):
+        inputs = []
+        for i in range(bs):
+            inputs.append(np.random.choice(v, k, replace=False))
+        inputs = torch.from_numpy(np.asarray(inputs)).long().cuda()
+        # inputs, _ = torch.sort(inputs)  # make what I am optimizing more clear but then the targets are in order
+        _, targets = torch.sort(inputs, dim=-1)
+
+        if _print:
+            print(inputs)
+            print(targets)
+            exit()
+
+        return inputs, targets
+
+
+def train():
+    num_class = 19
+    max_k = 11
+    dim = 64
+    bs = 10
+
+    model = RankModel(num_class, max_k, dim).cuda()
+    optimizer = SGD(model.parameters(), lr=0.01)
+
+    pl = PlackettLuceLoss()
+
+    for i in range(90):  # larger values might diverge due to numerical stability issue with eta
+        inputs, targets = model.make_inputs_targets(bs, max_k+1, num_class)
+        logits = model(inputs)
+
+        # print(inputs[:1, :])
+        # print(targets[:1, :])
+        # print(logits[:1, :])
+
+        # cut off here is because algorithm is not tested for full ranks
+        loop_pl_avg_loss, loop_pl_full_loss, _ = pl.loop_forward(logits, pl_targets=targets)
+
+        pl_avg_loss, pl_full_loss, _ = pl(logits, pl_targets=targets)
+
+        print(loop_pl_full_loss[:1, :])
+        print(pl_full_loss[:1, :])
+
+        ce_loss = nn.functional.cross_entropy(logits, targets[..., 0])
+        #ce_loss.backward()
+
+        pl_avg_loss.backward()
+        optimizer.step()
+
+        print(f'Loss at {i}: {pl_avg_loss.item()}, {ce_loss.item()}')
+        # print(f'Loss at {i}: {ce_loss.item()}')
+        print()
 
 
 if __name__ == '__main__':
-    pass
+    train()